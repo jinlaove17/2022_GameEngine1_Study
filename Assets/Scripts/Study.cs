5f69c688ee5eafecf152b69af8e94e746f71ca30
DESKTOP-6933JOM\전종우 <jinlaove17@naver.com>
2022-03-18 11:40:54 +0900

    [코드 수정] 간단한 타이머 코드 실습

--- Assets/Scripts/Study.cs
+++ Assets/Scripts/Study.cs
@@ -3,49 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable] // 에디터에서 편집 가능해짐
-public class ComplexType
-{
-    public string m_name;
-    public int m_age;
-    public float m_height;
-}
-
 public class Study : MonoBehaviour
 {
-    public ComplexType m_studyName;
+    private const float m_attackInterval = 3.0f;
 
-    private void Start()
-    {
-        print(message: "게임이 시작되었습니다.");
-    }
-
-    private void OnEnable()
-    {
-        // 이 컴포넌트가 활성화될 때 실행되는 함수(게임 오브젝트가 활성화되는 경우에도 실행됨)
-        print(message: "컴포넌트가 활성화 되었습니다.");
-    }
-
-    private void OnDisable()
-    {
-        // 이 컴포넌트가 비활성화될 때 실행되는 함수(게임 오브젝트가 비활성화되는 경우에도 실행됨)
-        print(message: "컴포넌트가 비활성화 되었습니다.");
-    }
-
-    private void FixedUpdate()
-    {
-        // 물리적인 연산을 수행하기 위해 주기적으로 실행
-        print(message: "컴포넌트가 주기적으로 업데이트 되었습니다.");
-    }
+    private float m_attackIntervalTimer;
 
     private void Update()
     {
-        print(message: "컴포넌트가 업데이트 되었습니다.");
+        m_attackIntervalTimer += Time.deltaTime;
+
+        if (m_attackIntervalTimer >= 3.0f)
+        {
+            PrintText();
+            m_attackIntervalTimer = 0.0f;
+        }
     }
 
-    private void LateUpdate()
+    private void PrintText()
     {
-        // UI나 카메라를 이동시키거나 사용자의 입력을 받은 후 사용
-        print(message: "업데이트 후에 실행됩니다.");
+        print(message: "Attack!");
     }
 }
