386f265e92b3fdb4e9510b4108c653628782b721
DESKTOP-6933JOM\전종우 <jinlaove17@naver.com>
2022-05-20 10:25:06 +0900

    [코드 추가] JSON .NET을 이용한 직렬화

--- Assets/Scripts/Serializer/Data.cs
+++ Assets/Scripts/Serializer/Data.cs
@@ -2,17 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
 
 [Serializable]
 public class Data
 {
-    public string                  dataName;
-    public int                     age;
-    public float                   height;
-    public Vector3                 playerPosition;
-    public Quaternion              playerRotation;
+    public string                    dataName;
+    public int                       age;
+    public float                     height;
+    public Vector3                   playerPosition;
+    public Vector3                   playerRotation;
 
     // 유니티에서 제공하는 JsonUntility는 아래와 같은 컨테이너들은 직렬화하지 못한다.
-    //public Dictionary<string, int> someDictionary;
-    //public Queue<string>           someQueue;
+    public Dictionary<string, int> someDictionary;
+
+    // JsonIgnore : 직렬화에서 제외하고 싶은 경우
+    [JsonIgnore]
+    public Queue<string>           someQueue;
 }
--- Assets/Scripts/Serializer/Serializer.cs
+++ Assets/Scripts/Serializer/Serializer.cs
@@ -1,6 +1,9 @@
+using System.IO;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class Serializer : MonoBehaviour
 {
@@ -12,10 +15,21 @@ public class Serializer : MonoBehaviour
             age = 100,
             height = 1.8f,
             playerPosition = new Vector3(10.0f, 0.0f, 0.0f),
-            playerRotation = Quaternion.Euler(10.0f, 20.0f, 30.0f)
+            playerRotation = transform.rotation.eulerAngles,
+            someDictionary = new Dictionary<string, int>()
         };
 
-        string jsonString = JsonUtility.ToJson(data);
+        data.someDictionary.Add("Hi", 10);
+
+        string jsonString = JsonConvert.SerializeObject(data);
+
+        // using scope 안에서 파일 스트림을 열면, 별도로 닫을 필요가 없다.
+        using (FileStream fileStream = new FileStream(Path.Combine(Application.persistentDataPath, "save.json"), FileMode.Create))
+        {
+            byte[] jsonToBytes = Encoding.UTF8.GetBytes(jsonString);
+
+            fileStream.Write(jsonToBytes);
+        }
 
         print(jsonString);
     }
